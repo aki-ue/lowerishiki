d97cda21cc931dbaf834492beb9ff15a04e3bcff
Akihiro Isotani <67610636+aki-ue@users.noreply.github.com>
2021-02-22 23:18:31 +0900

    Update replaceishiki.js

--- replaceishiki.js
+++ replaceishiki.js
@@ -1,9 +1,11 @@
-tags ={p,h1,...,h6}
-for(let i=0;i<arr.length;++i){
-	var matches = document.queryselectorall(tags[i]);
-	for(var j=0;i<matches.length;++i){
+let tags =["p","h1","h2","h3","h4","h5","h6"]
+for(let i=0;i<tags.length;++i){
+	var matches = document.querySelectorAll(tags[i]);
+	for(var j=0;j<matches.length;++j){
 		var str = matches[j].innerHTML;
-		str=str.replace("before","after");
-		matches[i].innerHTML=str
+		str=str.replace("エクセレント","卓越した");
+		str=str.replace("エクセレント","卓越した");
+		str=str.replace("ブルーオーシャン","未開拓市場")
+		matches[j].innerHTML=str
 	}
 }
